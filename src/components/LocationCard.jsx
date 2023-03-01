18e7d1969e2a212f712c8b04a6c43040c71ebbf4
Astern2112 <sternaarond@gmail.com>
2023-03-01 15:28:11 -0500

    added residents#

--- src/components/LocationCard.jsx
+++ src/components/LocationCard.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 const LocationCard = ({ location }) => {
   return (
     <div>
-      <div className="flex aspect-square flex-col items-center justify-center rounded-full bg-gradient-to-br from-blue-700 to-purple-500 p-10 font-bold text-white">
+      <div className="animate- flex aspect-square flex-col items-center justify-center rounded-full bg-gradient-to-b from-green-600 to-amber-800 p-10 font-bold text-white duration-150 hover:scale-105 hover:ease-in-out">
         <h3 className="text-2xl font-bold">{location.name}</h3>
         <p className="text-base font-medium">Type: {location.type}</p>
         <p className="text-base font-medium">Dimension: {location.dimension}</p>
+        <p className="text-base font-medium">
+          #Residents: {location.residents.length}
+        </p>
       </div>
     </div>
   );
