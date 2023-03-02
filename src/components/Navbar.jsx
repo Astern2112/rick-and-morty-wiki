6f73cf8093800fd7b307d6d5051d4320ee8ab2a1
Astern2112 <sternaarond@gmail.com>
2023-03-01 20:55:50 -0500

    made responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import { FaBars } from 'react-icons/fa';
+import { IoIosClose } from 'react-icons/io';
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="mx-9">
-      <ul className="hidden flex-wrap justify-center gap-12 text-lg md:flex md:justify-start md:gap-6">
-        <li>
-          <Link to="/characters">Characters</Link>
-        </li>
-        <li>
-          <Link to="/episodes">Episodes</Link>
-        </li>
-        <li>
-          <Link to="/locations">Locations</Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="mx-9">
+        <FaBars
+          onClick={() => toggleOpen()}
+          className="scale-150 hover:cursor-pointer md:hidden"
+        />
+        <ul className="hidden flex-wrap justify-center gap-12 text-lg md:flex md:justify-start md:gap-6">
+          <li className="hover:text-orange-500">
+            <Link to="/characters">Characters</Link>
+          </li>
+          <li className="hover:text-orange-500">
+            <Link to="/episodes">Episodes</Link>
+          </li>
+          <li className="hover:text-orange-500">
+            <Link to="/locations">Locations</Link>
+          </li>
+        </ul>
+      </nav>
+      {isOpen && (
+        <nav className="absolute right-0 top-0 min-h-max w-2/5 rounded-xl bg-gray-900 shadow-lg md:hidden ">
+          <IoIosClose
+            className="absolute right-6 top-3 scale-150 hover:cursor-pointer"
+            size={40}
+            onClick={() => toggleOpen()}
+          />
+          <ul className="flex flex-col justify-start gap-4 py-28 pl-5 text-left text-xl">
+            <li className="max-w-max hover:text-orange-500">
+              <Link to="/characters" onClick={() => toggleOpen()}>
+                Characters
+              </Link>
+            </li>
+            <li className="max-w-max hover:text-orange-500">
+              <Link to="/episodes" onClick={() => toggleOpen()}>
+                Episodes
+              </Link>
+            </li>
+            <li className="max-w-max hover:text-orange-500">
+              <Link to="/locations" onClick={() => toggleOpen()}>
+                Locations
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </>
   );
 };
 
